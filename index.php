678a78183a1a7c96e967f41f84a64d551fee8bb6
foresitegroup <mark@foresitegrp.com>
2016-04-15 15:59:20 -0500

    2016-04-15

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    
+    <title>Drwichman.com<?php if ($PageTitle != "") echo " | " . $PageTitle; ?></title>
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Foresite Group">
+    
+    <meta name="viewport" content="width=device-width">
+    <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,600,700|Open+Sans:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="inc/main.css">
+    
+    <script type="text/javascript" src="inc/jquery-1.12.0.min.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function() {
+        $("a[href^='http'], a[href$='.pdf']").not("[href*='" + window.location.host + "']").attr('target','_blank');
+      });
+    </script>
+  </head>
+  <body>
+    
+    <div class="dw-header">
+      <div class="site-width">
+        <div class="header-left">
+          <a href="."><img src="images/logo.png" alt="Drwichman.com"></a>
+        </div>
+        
+        <div class="header-right">
+          <div class="top-menu">
+            FOR APPOINTMENTS: <span>(888) 123-4567</span>
+          </div>
+          
+          <div class="menu">
+            <ul>
+              <li>
+                <a href="#">OUR PRACTICE</a>
+                <ul>
+                  <li><a href="#">Mark Wichman, MD</a></li>
+                  <li><a href="#">Matthew Wichman, MD</a></li>
+                  <li><a href="#">Hours &amp; Location</a></li>
+                  <li><a href="#">Schedule an Appointment</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">SPORTS MEDICINE</a>
+                <ul>
+                  <li><a href="#">Knee</a></li>
+                  <li><a href="#">Shoulder</a></li>
+                  <li><a href="#">Hip</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">EDUCATION</a>
+                <ul>
+                  <li><a href="#">Learning Center</a></li>
+                  <li><a href="#">Media</a></li>
+                  <li><a href="#">Medical Definitions &amp; Links</a></li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">PATIENT CARE</a>
+                <ul>
+                  <li><a href="#">Diagnosis</a></li>
+                  <li><a href="#">Pre-Op Care</a></li>
+                  <li><a href="#">Procedures</a></li>
+                  <li><a href="#">Post-Op Care</a></li>
+                </ul>
+              </li>
+              <li><a href="#">CONTACT</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="banner">
+      <div class="site-width">
+        BANNER (FULL WIDTH)
+      </div>
+    </div>
+
+    <div class="below-banner">
+      <div class="site-width">
+        BELOW BANNER (FULL WIDTH / THREE COL)
+      </div>
+    </div>
+
+    <div class="wichmans">
+      <div class="site-width">
+        WICHMANS IMAGES / PROFILES (FULL WIDTH)
+      </div>
+    </div>
+    
+    <div class="site-width">
+      CONTENT (TWO COL)
+    </div>
+
+    <div class="news-home">
+      <div class="site-width">
+        NEWS / BLOG (FULL WIDTH / TWO COL)
+      </div>
+    </div>
+
+    <div class="testimonials">
+      <div class="site-width">
+        ROTATING TESTIMONIALS (FULL WIDTH / TWO COL)
+      </div>
+    </div>
+    
+    <div class="dw-footer">
+      <div class="site-width">
+        FOOTER MENU<br>
+        &copy; <?php echo date("Y"); ?> Drwichman.com
+      </div>
+    </div>
+    
+  </body>
+</html>
