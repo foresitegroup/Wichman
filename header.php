18e61c11cdfb8917fa2ab99d74baa7c121adf33c
foresitegroup <mark@foresitegrp.com>
2016-06-28 13:58:20 -0500

    Add filemtime

--- header.php
+++ header.php
@@ -18,7 +18,7 @@ if (!isset($TopDir)) $TopDir = "";
     <meta name="viewport" content="width=device-width">
     <link href='https://fonts.googleapis.com/css?family=Titillium+Web:200,400,600,700|Open+Sans:400,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="<?php echo $TopDir; ?>inc/main.css">
+    <link rel="stylesheet" href="<?php echo $TopDir; ?>inc/main.css?<?php echo filemtime(realpath(dirname(__FILE__)) . "/inc/main.css"); ?>">
 
     <script type="text/javascript" src="<?php echo $TopDir; ?>inc/jquery-1.12.0.min.js"></script>
     <script type="text/javascript">
